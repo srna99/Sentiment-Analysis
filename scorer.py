6a98b79511cc53380c9b15e421b5fc368be8e56d
Serena Cheng <serena.ch1999@gmail.com>
2020-04-13 17:18:11 -0400

    Finish general running of programs.

--- scorer.py
+++ scorer.py
@@ -16,93 +16,95 @@ Ex. python3 scorer.py my-line-answers.txt line-key.txt
     > phone     64     8
     > product    4    50
 Algorithm:
-Each sense in the answers set is compared to the sense in the key and then
+Each sentiments in the answers set is compared to the sentiments in the key and then
 inputted in the confusion matrix. The baseline accuracy is calculated by
-assuming every sense to be the most frequent one (in this case it is "phone") and
+assuming every sentiments to be the most frequent one (in this case it is "phone") and
 dividing the number of correct instances by the total number of answers. The
-overall accuracy is calculated by dividing the total correct senses by the
+overall accuracy is calculated by dividing the total correct sentiments by the
 total number of answers.
 """
 
 import sys
 import re
 
-line_answers = sys.argv[1]  # answer set file
-line_key = sys.argv[2]  # test key file
+test_answers = sys.argv[1]  # answer set file
+test_key = sys.argv[2]  # test key file
 
 answer_content = []  # test tokenized
 key_content = []  # key tokenized
 
-answers = []  # senses in answer
-key = []  # senses in key
+answers = {}  # sentiments in answer
+key = {}  # sentiments in key
 
-senses = {"phone": 0, "product": 1}  # associate sense with number
+sentiments = {"positive": 0, "negative": 1}  # associate sentiments with number
 
 
-# split senses from instances and add to respective sense lists
-def get_senses(answer_list, instance_sense_list):
+# split sentiments from instances and add to respective sentiments lists
+def get_sentiments(answer_list, instance_sentiment_dict):
     for ans in answer_list:
-        sense = re.search(r'senseid=\"(.*)\"', ans).group(1)
-        instance_sense_list.append(sense)
+        ans_parts = re.search(r'instance=\"(.*)\" sentiment=\"(.*)\"', ans)
+        instance_id = ans_parts.group(1)
+        sentiment = ans_parts.group(2)
+        instance_sentiment_dict[instance_id] = sentiment
 
 
 # tokenize answers
-with open(line_answers, 'r', encoding="utf-8-sig") as file:
+with open(test_answers, 'r', encoding="utf-8-sig") as file:
     answer_content.extend(file.read().split("\n"))
     answer_content.pop()
 
 # tokenize key
-with open(line_key, 'r', encoding="utf-8-sig") as file:
+with open(test_key, 'r', encoding="utf-8-sig") as file:
     key_content.extend(file.read().split("\n"))
     key_content.pop()
 
-# get only senses
-get_senses(answer_content, answers)
-get_senses(key_content, key)
+# get only sentiments
+get_sentiments(answer_content, answers)
+get_sentiments(key_content, key)
 
-# create matrix filled with 0s that is sized (num of senses x num of senses)
-confusion_matrix = [[0] * len(senses) for _ in range(len(senses))]
+# create matrix filled with 0s that is sized (num of sentiments x num of sentiments)
+confusion_matrix = [[0] * len(sentiments) for _ in range(len(sentiments))]
 
-most_sense = ''  # most frequent sense
-most_count = 0  # correct count of frequent sense
-total_correct = 0  # total correct count of all senses
-for ind in range(len(key)):
-    actual_sense = key[ind]  # actual
-    predicted_sense = answers[ind]  # predicted
+most_sentiment = ''  # most frequent sentiment
+most_count = 0  # correct count of frequent sentiment
+total_correct = 0  # total correct count of all sentiments
+for instance_id in key.keys():
+    actual_sentiment = key[instance_id]  # actual
+    predicted_sentiment = answers[instance_id]  # predicted
 
     # increment on matrix
-    confusion_matrix[senses[actual_sense]][senses[predicted_sense]] += 1
+    confusion_matrix[sentiments[actual_sentiment]][sentiments[predicted_sentiment]] += 1
 
-    if actual_sense == predicted_sense:
-        # update if sense count is higher than current highest count
-        if confusion_matrix[senses[actual_sense]][senses[predicted_sense]] > most_count:
-            most_sense = actual_sense
-            most_count = confusion_matrix[senses[actual_sense]][senses[predicted_sense]]
+    if actual_sentiment == predicted_sentiment:
+        # update if sentiments count is higher than current highest count
+        if confusion_matrix[sentiments[actual_sentiment]][sentiments[predicted_sentiment]] > most_count:
+            most_sentiment = actual_sentiment
+            most_count = confusion_matrix[sentiments[actual_sentiment]][sentiments[predicted_sentiment]]
 
-        # increment total correct count if sense is correct
+        # increment total correct count if sentiments is correct
         total_correct += 1
 
-# BA = (num of correct answers if all are most frequent sense)/total num of answers
+# BA = (num of correct answers if all are most frequent sentiments)/total num of answers
 baseline_accuracy = round(most_count / len(key), 2)
 # A = num of correct answers/total num of answers
 accuracy = round(total_correct / len(key), 2)
 
-print("Baseline Accuracy (phone):", baseline_accuracy)
+print("Baseline Accuracy (" + most_sentiment + ") :", baseline_accuracy)
 print("Overall Accuracy:", accuracy)
 
-ordered_tags = [''] * len(senses)
-# put senses in order
-for tag, num in senses.items():
+ordered_tags = [''] * len(sentiments)
+# put sentiments in order
+for tag, num in sentiments.items():
     ordered_tags[num] = tag
 
-# print all senses horizontally
+# print all sentiments horizontally
 for ind in range(len(ordered_tags)):
     if ind == 0:
-        print("\n        ", end='')
+        print("\n         ", end='')
     print(format(ordered_tags[ind], '>4'), end=' ')
 
-# print all senses and counts in matrix
-for ind in range(len(senses)):
-    print("\n", format(ordered_tags[ind], '7'), end='')
-    for j in range(len(senses)):
-        print(format(confusion_matrix[ind][j], '5'), end=' ')
+# print all sentiments and counts in matrix
+for ind in range(len(sentiments)):
+    print("\n", format(ordered_tags[ind], '8'), end='')
+    for j in range(len(sentiments)):
+        print(format(confusion_matrix[ind][j], '8'), end=' ')
--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,6 @@
 """
 Serena Cheng
-CMSC 416 - PA 5: Sentiment Analysis (sentiment.py)
+CMSC 416 - PA 5: Sentiment Analysis (sentiments.py)
 4/14/2020
 ~~~~~
 Problem:
@@ -14,10 +14,10 @@ Ex. python3 wsd.py line-train.txt line-test.txt my-model.txt > my-line-answers.t
 Algorithm:
 This model is based on Yarowsky's decision list. Unigrams and bi-grams surrounding the target word are
 collected as features and their position relative to the target word is noted. Features used includes w+1,
-w-1, w-2 & w-1, w+1 & w+2, and several more. Smoothing was applied, where each feature's sense frequency
+w-1, w-2 & w-1, w+1 & w+2, and several more. Smoothing was applied, where each feature's sentiments frequency
 was increased by 1. Tests based on the features were created and ranked based on their log-likelihood score
 (abs(log( (P(sense_1|feature)/(P(sense_2|feature) ))). The tests are then applied to the test data in
-ranked order, where the first test to pass results in the associated sense.
+ranked order, where the first test to pass results in the associated sentiments.
 Baseline Accuracy (phone): 0.51
 Overall Accuracy: 0.9
         phone product
@@ -36,25 +36,13 @@ model = sys.argv[3]  # model log file
 train_content = []  # tokenized training set
 test_content = []  # tokenized test set
 
-# senses
-sense_phone = "phone"
-sense_product = "product"
+# sentiments
+sentiment_positive = "positive"
+sentiment_negative = "negative"
 
-# feature types
-w_plus_1 = "+1"  # w+1
-w_minus_1 = "-1"  # w-1
-w_pair_minus_2 = "-2 & -1"  # w-2 & w-1
-w_pair_plus_2 = "+1 & +2"  # w+1 & w+2
-w_pair_minus_plus_1 = "-1 & +1"  # w-1 & w+1
-w_window_3 = "k=3"  # +=3
-w_window_5 = "k=5"  # +=5
-w_window_10 = "k=10"  # +=10
-
-# sense frequency based on features
-feature_sense_dict = {w_plus_1: {}, w_minus_1: {}, w_pair_minus_2: {}, w_pair_plus_2: {},
-                      w_pair_minus_plus_1: {}, w_window_3: {}, w_window_5: {}, w_window_10: {}}
+feature_sense_dict = {}  # sentiments frequency based on features
 feature_frequency_dict = {}  # feature frequency
-sense_frequency_dict = {sense_phone: 0, sense_product: 0}  # sense frequency
+sense_frequency_dict = {sentiment_positive: 0, sentiment_negative: 0}  # sentiments frequency
 
 ranked_tests = []  # ranked tests
 answers = []  # answers to test set
@@ -64,65 +52,7 @@ answers = []  # answers to test set
 def clean_context(content):
     content = re.sub(r'(<.>|<\/.>|\.|,|;|!|-|&|\)|\(|\"|\?)', ' ', content)
     content = re.sub(r'\s+', ' ', content)
-    content = re.sub(">lines<", ">line<", content)
     return content.split()
-
-
-# adds and updates counts of senses according to features
-def add_to_feature_sense_dict(w_feature, feature_word):
-    if feature_word not in feature_sense_dict[w_feature].keys():
-        feature_sense_dict[w_feature][feature_word] = {sense_phone: 1, sense_product: 1}
-
-    feature_sense_dict[w_feature][feature_word][sense] += 1
-
-
-# gets all unigrams within a window and updates frequencies
-def add_with_window_size(context_list, w_feature, k, target_ind, add_frequencies):
-    for ind in range(target_ind - k, target_ind + k):
-        # skip index if out of range or is target
-        if ind < 0 or ind >= len(context_list) or ind == target_ind:
-            continue
-
-        add_to_feature_sense_dict(w_feature, context_list[ind])
-
-        # updates frequencies
-        if add_frequencies:
-            increment_feature_frequency(context_list[ind])
-
-
-# adds and updates counts of features
-def increment_feature_frequency(feature_term):
-    if feature_term in feature_frequency_dict.keys():
-        feature_frequency_dict[feature_term] += 1
-    else:
-        feature_frequency_dict[feature_term] = 1
-
-
-# creates test based on info from ranked tests list and returns if the test was a success or a failure
-def create_test(test_number, feature_term, is_list):
-    test_feature = ranked_tests[test_number]  # test info according to number
-
-    # if training feature is in tested feature list then success
-    if is_list and test_feature[2] in feature_term:
-        return True
-    # if tested feature is same as training feature then success
-    elif not is_list and feature_term == test_feature[2]:
-        return True
-
-    # if test failed then failure
-    return False
-
-
-# creates list of features of window size from test data
-def create_test_feature_list(context_list, k, target_ind):
-    features_list = []
-    for i in range(target_ind - k, target_ind + k):
-        # skip if out of range or is target
-        if i < 0 or i >= len(context_list) or i == target_ind:
-            continue
-        features_list.append(context_list[i])
-
-    return features_list
 
 
 # tokenize training set by instance and pop off useless last bit
@@ -137,67 +67,45 @@ with open(test_set, 'r', encoding="utf-8-sig") as file:
 
 # for training set
 for instance in train_content:
-    # get sense and update count
-    sense = re.search(r'senseid=\"(.*)\"', instance).group(1)
-    sense_frequency_dict[sense] += 1
+    # get sentiments and update count
+    sentiment = re.search(r'sentiment=\"(.*)\"', instance).group(1)
+    sense_frequency_dict[sentiment] += 1
 
     # get context then clean and tokenize it
     context = re.search(r'<context>\n(.*)\n</context>', instance).group(1)
     context_words = clean_context(context)
 
-    target = context_words.index("<head>line</head>")  # index of target
+    # get features
+    for feature_word in context_words:
+        if feature_word not in feature_sense_dict.keys():
+            feature_sense_dict[feature_word] = {sentiment_positive: 1, sentiment_negative: 1}
 
-    # get features of type w+1
-    if target + 1 < len(context_words):
-        add_to_feature_sense_dict(w_plus_1, context_words[target + 1])
+        feature_sense_dict[feature_word][sentiment] += 1
 
-    # get features of type w-1
-    if target - 1 >= 0:
-        add_to_feature_sense_dict(w_minus_1, context_words[target - 1])
-
-    # get features of type w-2 & w-1
-    if target - 2 >= 0:
-        bigram = context_words[target - 2] + " " + context_words[target - 1]
-        add_to_feature_sense_dict(w_pair_minus_2, bigram)
-        increment_feature_frequency(bigram)
-
-    # get features of type w+1 & w+2
-    if target + 2 < len(context_words):
-        bigram = context_words[target + 1] + " " + context_words[target + 2]
-        add_to_feature_sense_dict(w_pair_plus_2, bigram)
-        increment_feature_frequency(bigram)
-
-    # get features of type w-1 & w+1
-    if target - 1 >= 0 and target + 1 < len(context_words):
-        bigram = context_words[target - 1] + " " + context_words[target + 1]
-        add_to_feature_sense_dict(w_pair_minus_plus_1, bigram)
-        increment_feature_frequency(bigram)
-
-    # get features of window sizes 3, 5, and 10
-    add_with_window_size(context_words, w_window_3, 3, target, False)
-    add_with_window_size(context_words, w_window_5, 5, target, False)
-    add_with_window_size(context_words, w_window_10, 10, target, True)
+        if feature_word in feature_frequency_dict.keys():
+            feature_frequency_dict[feature_word] += 1
+        else:
+            feature_frequency_dict[feature_word] = 1
 
 # calculate log-likelihood ratio for each feature
-for feature_type, feature_list in feature_sense_dict.items():
-    for feature, frequency in feature_list.items():
-        # probability of phone given feature
-        prob_phone = frequency[sense_phone] / feature_frequency_dict[feature]
-        # probability of product given feature
-        prob_product = frequency[sense_product] / feature_frequency_dict[feature]
+for feature, sentiment_frequencies in feature_sense_dict.items():
+    # probability of phone given feature
+    prob_pos = sentiment_frequencies[sentiment_positive] / feature_frequency_dict[feature]
+    # probability of product given feature
+    prob_neg = sentiment_frequencies[sentiment_negative] / feature_frequency_dict[feature]
 
-        # pick sense with higher probability
-        if prob_phone >= prob_product:
-            sense = sense_phone
-        else:
-            sense = sense_product
+    # pick sentiments with higher probability
+    if prob_pos >= prob_neg:
+        sentiment = sentiment_positive
+    else:
+        sentiment = sentiment_negative
 
-        # abs(log( (P(sense_1|feature)/(P(sense_2|feature) ))
-        log_likelihood_ratio = round(abs(math.log(prob_phone / prob_product)), 5)
+    # abs(log( (P(sense_1|feature)/(P(sense_2|feature) ))
+    log_likelihood_ratio = round(abs(math.log(prob_pos / prob_neg)), 5)
 
-        # add calculated ratio, feature type, feature, and sense to test
-        test = (log_likelihood_ratio, feature_type, feature, sense)
-        ranked_tests.append(test)
+    # add calculated ratio, feature type, feature, and sentiments to test
+    test = (log_likelihood_ratio, feature, sentiment)
+    ranked_tests.append(test)
 
 # sort tests in descending order
 ranked_tests.sort(key=lambda x: x[0], reverse=True)
@@ -205,8 +113,8 @@ ranked_tests.sort(key=lambda x: x[0], reverse=True)
 # write info to model log
 with open(model, 'w', encoding="utf-8-sig") as file:
     for data in ranked_tests:
-        line = "feature type = " + data[1] + ", feature = " + data[2] + ", log-likelihood score = " + str(
-                data[0]) + ", sense = " + data[3] + "\n"
+        line = "feature = " + data[1] + ", sentiment = " + data[2] + ", log-likelihood score = " + str(
+                data[0]) + "\n"
         file.write(line)
 
 # for test set
@@ -218,53 +126,22 @@ for instance in test_content:
     context = re.search(r'<context>\n(.*)\n</context>', instance).group(1)
     context_words = clean_context(context)
 
-    target = context_words.index("<head>line</head>")  # index of target
+    sentiment = ""  # chosen sentiments
+    # going through tests in order and if there is a success, assign associated sentiments to target
+    for test in ranked_tests:
+        # if training feature is in test then success
+        if test[1] in context_words:
+            sentiment = test[2]
 
-    sense = ""  # chosen sense
-    # going through tests in order and if there is a success, assign associated sense to target and break
-    for ind, test in enumerate(ranked_tests):
-        if test[1] == w_plus_1 and target + 1 < len(context_words):
-            if create_test(ind, context_words[target + 1], False):
-                sense = test[3]
-                break
-        elif test[1] == w_minus_1 and target - 1 >= 0:
-            if create_test(ind, context_words[target - 1], False):
-                sense = test[3]
-                break
-        elif test[1] == w_pair_minus_2 and target - 2 >= 0:
-            if create_test(ind, context_words[target - 2] + " " + context_words[target - 1], False):
-                sense = test[3]
-                break
-        elif test[1] == w_pair_plus_2 and target + 2 < len(context_words):
-            if create_test(ind, context_words[target + 1] + " " + context_words[target + 2], False):
-                sense = test[3]
-                break
-        elif test[1] == w_pair_minus_plus_1 and target - 1 >= 0 and target + 1 < len(context_words):
-            if create_test(ind, context_words[target - 1] + " " + context_words[target + 1], False):
-                sense = test[3]
-                break
-        elif test[1] == w_window_3:
-            if create_test(ind, create_test_feature_list(context_words, 3, target), True):
-                sense = test[3]
-                break
-        elif test[1] == w_window_5:
-            if create_test(ind, create_test_feature_list(context_words, 5, target), True):
-                sense = test[3]
-                break
-        elif test[1] == w_window_10:
-            if create_test(ind, create_test_feature_list(context_words, 10, target), True):
-                sense = test[3]
-                break
-
-    # if no sense was assigned and every test failed, assign the most frequent sense to target
-    if sense == "":
-        if sense_frequency_dict[sense_phone] >= sense_frequency_dict[sense_product]:
-            sense = sense_phone
+    # if no sentiments was assigned and every test failed, assign the most frequent sentiments to target
+    if sentiment == "":
+        if sense_frequency_dict[sentiment_positive] >= sense_frequency_dict[sentiment_negative]:
+            sentiment = sentiment_positive
         else:
-            sense = sense_product
+            sentiment = sentiment_negative
 
     # format answer and add to list
-    answer = "<answer instance=\"" + instance_id + "\" senseid=\"" + sense + "\"/>"
+    answer = "<answer instance=\"" + instance_id + "\" sentiment=\"" + sentiment + "\"/>"
     answers.append(answer)
 
 # print answers
